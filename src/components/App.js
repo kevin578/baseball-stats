7ebcec39ac9a1c18499e0a6e8f9ac9e95e777357
kevin578 <kevinbriggs1@gmail.com>
2019-05-08 22:34:07 -0400

    everything is sorted

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,46 @@
-import React, {useEffect} from "react";
-import axios from 'axios';
-import data from '../data/1998_stats.json';
-import _ from 'lodash';
+import React, { useEffect } from "react";
+import axios from "axios";
+import stats from '../data/ranked_stats.json'
 
 
 
 const App = () => {
-    const players = {
-        AL_EAST: [],
-        AL_CENTRAL: [],
-    }
-    useEffect(()=> {
-        let obj = JSON.parse(data).SEASON.LEAGUE[0].DIVISION[0];
-        console.log(obj)
 
-    }, [])
+const getData = ()=> {
+    return stats.sb.descending.map((item, index)=> {
+        return (
+            <tr>  
+                <td >{item.full_name}</td>
+                <td>{item.avg}</td>
+                <td>{item.hr}</td>
+                <td>{item.rbi}</td>
+                <td>{item.runs}</td>
+                <td>{item.sb}</td>
+                <td>{item.ops}</td>
+            </tr>
+        )
+    })
+}
+  useEffect(() => {
 
+  }, []);
 
-  return <div>Hello</div>;
+  return (
+    <table>
+    <tbody>
+    <tr>
+      <td>Player Name</td>
+      <td>Avg</td>
+      <td>Home Runs</td>
+      <td>RBIs</td>
+      <td>Runs</td>
+      <td>SB</td>
+      <td>OPS</td>
+    </tr>
+    { getData() }
+    </tbody>
+    </table>
+  );
 };
 
 export default App;
