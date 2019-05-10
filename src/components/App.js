6e33adf9223f0a8f0428931554dc482c39b66ef5
kevin578 <kevinbriggs1@gmail.com>
2019-05-09 21:51:43 -0400

    styled table

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,94 @@
-import React, { useState } from "react";
-import axios from "axios";
-import stats from '../data/ranked_stats.json'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import short from 'short-uuid';
+import stats from '../data/ranked_stats.json';
 
+const Table = styled.table`
+  background: #eaf7ed;
+  margin-top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 700px;
+  border-collapse: collapse;
+  border-radius: 4px;
+  margin-bottom: 40px;
+`;
 
+const TitleRow = styled.tr`
+  height: 40px;
+`;
+
+const TitleCell = styled.th`
+  width: ${props => props.width};
+  text-align: left;
+  border-bottom: 2px solid #353535;
+  padding: 20px;
+  cursor: pointer;
+`;
+
+const Cell = styled.td`
+  height: 20px;
+  padding: 20px;
+`;
+
+const Row = styled.tr`
+  background: ${props =>
+    props.background === 'even' ? '#daefdf' : 'transparent'};
+`;
 
 const App = () => {
+  const [currentDataProperties, setCurrentDataProperties] = useState({
+    criteria: 'avg',
+    order: 'descending'
+  });
 
-  const [currentDataProperties, setCurrentDataProperties] = useState({criteria: "avg", order: "descending"});
-
-  const getData = ()=> {
-    const {criteria, order} = currentDataProperties;
+  const getData = () => {
+    const { criteria, order } = currentDataProperties;
     const currentData = stats[criteria][order];
 
-    return currentData.map((item, index)=> {
-        return (
-            <tr>  
-                <td>{item.full_name}</td>
-                <td>{item.avg}</td>
-                <td>{item.hr}</td>
-                <td>{item.rbi}</td>
-                <td>{item.runs}</td>
-                <td>{item.sb}</td>
-                <td>{item.ops}</td>
-            </tr>
-        )
-    })
-}
+    return currentData.map((item, index) => {
+      return (
+        <Row key={short.uuid()} background = {index % 2 === 0 ? "even":"odd"}>
+          <Cell>{item.full_name}</Cell>
+          <Cell>{item.avg}</Cell>
+          <Cell>{item.hr}</Cell>
+          <Cell>{item.rbi}</Cell>
+          <Cell>{item.runs}</Cell>
+          <Cell>{item.sb}</Cell>
+          <Cell>{item.ops}</Cell>
+        </Row>
+      );
+    });
+  };
 
-const handleSort = (criteria)=> {
-  //console.log(Object.keys)
-  setCurrentDataProperties({criteria, order: "descending"});
-}
+  const handleSort = criteria => {
+    let order = 'descending';
+    if (
+      criteria === currentDataProperties.criteria &&
+      currentDataProperties.order === 'descending'
+    ) {
+      order = 'ascending';
+    }
+    setCurrentDataProperties({ criteria, order });
+  };
 
+  
 
   return (
-    <table>
-    <tbody>
-    <tr>
-      <td>Player Name</td>
-      <td onClick = {()=> handleSort("hr")}>Avg</td>
-      <td onClick = {()=> handleSort("hr")}>HRs</td>
-      <td onClick = {()=> handleSort("rbi")}>RBIs</td>
-      <td onClick = {()=> handleSort("runs")}>Runs</td>
-      <td onClick = {()=> handleSort("sb")}>SB</td>
-      <td onClick = {()=> handleSort("ops")}>OPS</td>
-    </tr>
-    { getData() }
-    </tbody>
-    </table>
+    <Table>
+      <tbody>
+        <TitleRow>
+          <TitleCell>Player Name</TitleCell>
+          <TitleCell onClick={() => handleSort('avg')}>Avg</TitleCell>
+          <TitleCell onClick={() => handleSort('hr')}>HRs</TitleCell>
+          <TitleCell onClick={() => handleSort('rbi')}>RBIs</TitleCell>
+          <TitleCell onClick={() => handleSort('runs')}>Runs</TitleCell>
+          <TitleCell onClick={() => handleSort('sb')}>SB</TitleCell>
+          <TitleCell onClick={() => handleSort('ops')}>OPS</TitleCell>
+        </TitleRow>
+        {getData()}
+      </tbody>
+    </Table>
   );
 };
 
